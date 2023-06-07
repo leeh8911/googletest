d4c0e89f9b98b0f767117b7e2651b922f1be64dd
sangwon lee <leeh8911@gmail.com>
2023-06-08 06:38:30 +0900

    update premake file

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+﻿project "gtest"
+	kind "StaticLib"
+	language "C++"
+    cppdialect "C++14"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "googletest/src/**.cc",
+        "googletest/include/**.h",
+        "googlemock/src/**.cc",
+        "googlemock/include/**.h",
+	}
+
+    includedirs
+    {
+        "googletest",
+        "googlemock",
+        "googletest/include",
+        "googlemock/include",
+    }
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
